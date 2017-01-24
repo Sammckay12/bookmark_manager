40fa66d9a63e3f2585965263f1ad0cbbcf45ca5d
Mike Field-May <mfieldmay@gmail.com>
2017-01-24 16:39:49 +0000

    Added ruby version to gemfile

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 source "http://rubygems.org"
 
+ruby '2.3.3'
+
 # gem "rails"
 gem 'data_mapper'
 gem 'dm-migrations'
--- app/app.rb
+++ app/app.rb
@@ -4,6 +4,10 @@ require_relative 'models/link'
 
 class BookmarkApp < Sinatra::Base
 
+  get '/' do
+    redirect '/links'
+  end
+
   get '/links' do
     @links = Link.all
     erb :'links/index'
